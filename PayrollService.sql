9ee1e34eabe9811aa30b302c4166be65166609bb
ankitaraghuwanshi <bittanraghu@gmail.com>
2022-04-21 17:29:19 +0530

    [Ankita] Added: Sql program for creating Table

--- PayrollService.sql
+++ PayrollService.sql
@@ -2,3 +2,10 @@
 
 create database Payroll_Service
 use Payroll_Service
+
+create table EmployeePayroll(
+Emp_ID int identity(1,1) not null Primary key,
+Name varchar(50),
+Salary bigint,
+StartDate datetime
+)
