265690c8537810abd1bc03c68766b44b83c3994a
ankitaraghuwanshi <bittanraghu@gmail.com>
2022-04-23 14:39:15 +0530

    [Ankita] Added: program for generate connection

---
+++ Exception.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll
+{
+    public class Exceptions : Exception
+    {
+        ExceptionType type;
+        public enum ExceptionType
+        {
+            ConnectionFailed
+        }
+        public Exceptions(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
